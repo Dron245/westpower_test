951471d38891e0bb36057a75dca2d4bf19d0b84c
Dron245 <Dron245@yandex.ru>
2023-08-24 20:48:51 +0300

    24-08-2049

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,17 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+// const element = document.getElementById('tel');
+// const maskOptions = {
+//   mask: '+{7}(000)000-00-00'
+// };
+// const mask = IMask(element, maskOptions);
+
+// Inputmask({ mask: "99-999-99"}).mask(document.querySelectorAll('input-tel'));
+// import Inputmask from "inputmask";
+
+// Inputmask({"mask": "(999) 999-9999"}).mask(input-tel);
+// var Inputmask = require('inputmask');
+//es6
+// import Inputmask from "inputmask";
