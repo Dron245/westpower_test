90293e8f6b3efc9b6418a3d8c90c230ce90ae099
Dron245 <Dron245@yandex.ru>
2023-08-24 23:59:41 +0300

    2408-2359

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,16 +3,10 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-// const element = document.getElementById('tel');
-// const maskOptions = {
-//   mask: '+{7}(000)000-00-00'
-// };
-// const mask = IMask(element, maskOptions);
-
-// Inputmask({ mask: "99-999-99"}).mask(document.querySelectorAll('input-tel'));
-// import Inputmask from "inputmask";
-
-// Inputmask({"mask": "(999) 999-9999"}).mask(input-tel);
-// var Inputmask = require('inputmask');
-//es6
-// import Inputmask from "inputmask";
+let tel = document.querySelectorAll('input[type="tel"]');
+let maskOptions = {
+	mask: "+7(999)999-99-99",
+	
+}
+let im = new Inputmask(tel, maskOptions);
+im.mask(tel, maskOptions);
